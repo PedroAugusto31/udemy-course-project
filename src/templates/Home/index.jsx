49c87c4a8fec2a6334570f606b1fbd9a9518ea25
PedroAugusto31 <76188013+PedroAugusto31@users.noreply.github.com>
2021-11-26 18:11:49 -0300

    storyBook added

--- src/templates/Home/index.jsx
+++ src/templates/Home/index.jsx
@@ -1,10 +1,11 @@
+import { Heading } from "../../components/Heading";
 import * as Styled from "./styles";
 
 export function Home() {
   return (
     <div className="App">
       <Styled.Wrapper background="blue">
-        <h1>hello</h1>
+        <Heading>o texto maluuuuuco</Heading>
       </Styled.Wrapper>
     </div>
   );
