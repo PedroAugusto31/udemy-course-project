6210404afe9c468ce5f4c6af6589eab73c51057f
PedroAugusto31 <76188013+PedroAugusto31@users.noreply.github.com>
2021-11-26 23:08:06 -0300

    storyBook configured

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,3 +1,7 @@
+import { ThemeProvider } from "styled-components";
+import { GlobalStyles } from "../src/styles/globalStyles";
+import { theme } from "../src/styles/theme";
+
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
   controls: {
@@ -6,4 +10,17 @@ export const parameters = {
       date: /Date$/,
     },
   },
-}
+  background: {
+    default: "light",
+    
+  }
+};
+
+export const decorators = [
+  (Story) => (
+    <ThemeProvider theme={theme}>
+      <Story />
+      <GlobalStyles />
+    </ThemeProvider>
+  ),
+];
