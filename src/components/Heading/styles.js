aed8a70d4e58528cfec3a5699ef362c1d1592c7d
PedroAugusto31 <76188013+PedroAugusto31@users.noreply.github.com>
2021-11-30 17:15:16 -0300

    fixing some problems

--- src/components/Heading/styles.js
+++ src/components/Heading/styles.js
@@ -2,6 +2,6 @@ import styled, { css } from "styled-components";
 
 export const Title = styled.h1`
   ${({ theme, light }) => css`
-    color: ${light ? theme.colors.mainBg : theme.colors.blackTheme};
+    color: ${light ? theme.colors.mainBg : theme.colors.mainColors};
   `}
 `;
