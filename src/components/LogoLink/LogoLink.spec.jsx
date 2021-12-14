da29e2d215670ecc521a86f8f7eb3a881c1f315e
PedroAugusto31 <76188013+PedroAugusto31@users.noreply.github.com>
2021-12-13 22:16:46 -0300

    LogoLink tested

--- src/components/LogoLink/LogoLink.spec.jsx
+++ src/components/LogoLink/LogoLink.spec.jsx
@@ -4,9 +4,25 @@ import { renderTheme } from "../../utils/renderTheme";
 
 describe("<LogoLink />", () => {
   it("Should render text logo", () => {
+    renderTheme(<LogoLink link="#target" text="olá, mundo" />);
+    expect(
+      screen.getByRole("heading", { name: "olá, mundo" }).firstChild,
+    ).toHaveAttribute("href", "#target");
+  });
+  it("Should render image logo", () => {
+    renderTheme(
+      <LogoLink link="#target" text="olá, mundo" srcImg="image.png" />,
+    );
+    const logoLink = screen.getByRole("heading", { name: "olá, mundo" });
+    expect(screen.getByRole("img", { name: "olá, mundo" })).toHaveAttribute(
+      "src",
+      "image.png",
+    );
+  });
+  it("Should match snapshot", () => {
     const { container } = renderTheme(
       <LogoLink link="#target" text="olá, mundo" />,
     );
-    expect(container).toBeInTheDocument();
+    expect(container.firstChild).toMatchSnapshot();
   });
 });
