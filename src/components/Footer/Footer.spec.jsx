5faf2940c5c5057ddcb4669c88f9619fcedc9e65
PedroAugusto31 <76188013+PedroAugusto31@users.noreply.github.com>
2021-12-22 10:24:07 -0300

    goTop button created and tested

--- src/components/Footer/Footer.spec.jsx
+++ src/components/Footer/Footer.spec.jsx
@@ -5,7 +5,68 @@ import { renderTheme } from "../../utils/renderTheme";
 describe("<Footer />", () => {
   it("Should do something", () => {
     const { container } = renderTheme(<Footer html="<h1>olá</h1>" />);
-    expect(screen.getByText("Children")).toBeInTheDocument();
-    expect(container).toMatchInlineSnapshot();
+    expect(screen.getByRole("heading")).toBeInTheDocument();
+    expect(container).toMatchInlineSnapshot(`
+      .c2 {
+        text-align: center;
+      }
+
+      .c2 a {
+        color: inherit;
+        -webkit-text-decoration: none;
+        text-decoration: none;
+        font-size: 1.6rem;
+      }
+
+      .c0 {
+        background: #FFFFFF;
+        color: #0A1128;
+        min-height: 100vh;
+        display: -webkit-box;
+        display: -webkit-flex;
+        display: -ms-flexbox;
+        display: flex;
+        -webkit-align-items: center;
+        -webkit-box-align: center;
+        -ms-flex-align: center;
+        align-items: center;
+      }
+
+      .c1 {
+        max-width: 120rem;
+        margin: 0 auto;
+        padding: 3.2rem;
+      }
+
+      .c3 {
+        font-size: 2.4rem;
+      }
+
+      <div>
+        <div
+          class="c0"
+        >
+          <div
+            class="c1"
+          >
+            <footer
+              class="c2"
+            >
+              <div
+                class="c1"
+              >
+                <div
+                  class="c3"
+                >
+                  <h1>
+                    olá
+                  </h1>
+                </div>
+              </div>
+            </footer>
+          </div>
+        </div>
+      </div>
+    `);
   });
 });
--- src/components/GoTop/index.jsx
+++ src/components/GoTop/index.jsx
@@ -1,10 +1,11 @@
 import P from "prop-types";
 import * as Styled from "./styles";
+import { KeyboardArrowUp } from "@styled-icons/material-outlined/KeyboardArrowUp";
 
-export function GoTop({ children }) {
-  return <Styled.Container>{children}</Styled.Container>;
+export function GoTop() {
+  return (
+    <Styled.Container href="#" aria-label="Go to top">
+      <KeyboardArrowUp />
+    </Styled.Container>
+  );
 }
-
-GoTop.propTypes = {
-  children: P.node.isRequired,
-};
