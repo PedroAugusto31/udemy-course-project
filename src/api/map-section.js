a40695a84ed497eed6090baab114a00744dcbb98
PedroAugusto31 <76188013+PedroAugusto31@users.noreply.github.com>
2022-02-17 00:29:23 -0300

    data mapping finished

--- src/api/map-section.js
+++ src/api/map-section.js
@@ -16,6 +16,7 @@ export function mapSection(sections = []) {
         return mapImageGrid(section);
       }
     }
+    return section;
   });
 }
 
--- src/api/map-section.spec.js
+++ src/api/map-section.spec.js
@@ -23,6 +23,8 @@ describe("map-section", () => {
       },
     ]);
     const withoutComponent = mapSection([{}]);
+    expect(data).toEqual([{ __component: "section.section-grid" }]);
+    expect(withoutComponent).toEqual([{}]);
   });
 
   it("should map a section two columns with no data", () => {
@@ -38,42 +40,15 @@ describe("map-section", () => {
   it("should map a section two columns", () => {
     const sectionObj = mapSectionTwoColumns({
       __component: "section.section-two-columns",
-      _id: "602fdf2d540c00269e056178",
       title: "title",
       description: "abc",
       metadata: {
         background: true,
-        _id: "602fdf2d540c00269e05617b",
-        name: "contact",
         section_id: "contact",
-        __v: 0,
-        id: "602fdf2d540c00269e05617b",
       },
-      __v: 1,
       image: {
-        _id: "602fdc2b540c00269e05616a",
-        name: "javascript.svg",
-        alternativeText: "Desenho de pessoas segurando logos do CSS, JS e HTML",
-        caption: "Desenho de pessoas segurando logos do CSS, JS e HTML",
-        hash: "javascript_b57bf48cda",
-        ext: ".svg",
-        mime: "image/svg+xml",
-        size: 30.31,
         url: "a.svg",
-        provider_metadata: {
-          public_id: "javascript_b57bf48cda",
-          resource_type: "image",
-        },
-        provider: "cloudinary",
-        width: 1030,
-        height: 730,
-        related: ["602fdf2d540c00269e056178", "602fdf2d540c00269e056172"],
-        createdAt: "2021-02-19T15:41:31.808Z",
-        updatedAt: "2021-02-19T15:54:22.590Z",
-        __v: 0,
-        id: "602fdc2b540c00269e05616a",
       },
-      id: "602fdf2d540c00269e056178",
     });
     expect(sectionObj.background).toBe(true);
     expect(sectionObj.component).toBe("section.section-two-columns");
@@ -95,19 +70,12 @@ describe("map-section", () => {
   it("should map a section content", () => {
     const sectionContObj = mapSectionContent({
       __component: "section.section-content",
-      _id: "602fdf2d540c00269e056177",
       title: "Pricing",
       content: "abc",
       metadata: {
         background: false,
-        _id: "602fdf2d540c00269e056179",
-        name: "pricing",
         section_id: "pricing",
-        __v: 0,
-        id: "602fdf2d540c00269e056179",
       },
-      __v: 1,
-      id: "602fdf2d540c00269e056177",
     });
 
     expect(sectionContObj.background).toBe(false);
@@ -127,7 +95,8 @@ describe("map-section", () => {
     expect(sectionContObj.grid).toEqual([]);
   });
   it("should map a text grid with invalid data", () => {
-    const sectionContObj = mapTextGrid({ text_grid: [{}] });
+    const withoutComponent = mapTextGrid({ text_grid: [{}] });
+    expect(withoutComponent).not.toEqual({});
   });
   it("should map a text grid", () => {
     const sectionContObj = mapTextGrid({
@@ -136,39 +105,26 @@ describe("map-section", () => {
       title: "My Grid",
       text_grid: [
         {
-          _id: "602fdf2d540c00269e05617c",
           title: "Teste 1",
           description: "Coisa",
-          __v: 0,
-          id: "602fdf2d540c00269e05617c",
         },
         {
-          _id: "602fdf2d540c00269e05617d",
           title: "Teste 2",
           description:
             "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Debitis cum delectus molestias. Atque doloribus nobis laudantium esse ut, non commodi maxime distinctio veritatis unde, reprehenderit minus ad dolores provident maiores.",
-          __v: 0,
-          id: "602fdf2d540c00269e05617d",
         },
         {
-          _id: "602fdf2d540c00269e05617e",
           title: "Teste 3",
           description:
             "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Debitis cum delectus molestias. Atque doloribus nobis laudantium esse ut, non commodi maxime distinctio veritatis unde, reprehenderit minus ad dolores provident maiores.",
-          __v: 0,
-          id: "602fdf2d540c00269e05617e",
         },
       ],
       image_grid: [],
       metadata: {
         background: true,
-        _id: "602fdf2e540c00269e056199",
-        name: "grid-one",
+
         section_id: "grid-one",
-        __v: 0,
-        id: "602fdf2e540c00269e056199",
       },
-      __v: 2,
     });
 
     expect(sectionContObj.background).toBe(true);
@@ -189,163 +145,40 @@ describe("map-section", () => {
     expect(sectionContObj.description).toBe("");
     expect(sectionContObj.title).toBe("");
   });
-  it("should map a image grid with no data", () => {
+  it("should map a image grid with invalid data", () => {
     const sectionContObj = mapImageGrid({ image_grid: [{}] });
   });
   it("should map a image grid with data", () => {
     const sectionContObj = mapImageGrid({
       __component: "section.section-grid",
-      _id: "602fdf2d540c00269e056175",
       description: "abc",
       title: "Gallery",
       text_grid: [],
       image_grid: [
         {
-          _id: "602fdf2d540c00269e056183",
-          __v: 0,
           image: {
-            _id: "602fde37540c00269e05616b",
-            name: "http://source.unsplash.com/random/360x360?r=1",
             alternativeText: "abc",
-            caption: "",
-            hash: "360x360_r_1_6a2049d13a",
-            ext: "",
-            mime: "image/jpeg",
-            size: 29.09,
-            width: 360,
-            height: 360,
             url: "a.svg",
-            provider_metadata: {
-              public_id: "360x360_r_1_6a2049d13a",
-              resource_type: "image",
-            },
-            formats: {
-              thumbnail: {
-                name: "thumbnail_http://source.unsplash.com/random/360x360?r=1",
-                hash: "thumbnail_360x360_r_1_6a2049d13a",
-                ext: "",
-                mime: "image/jpeg",
-                width: 156,
-                height: 156,
-                size: 6.48,
-                path: null,
-                url: "https://res.cloudinary.com/dlizakp2a/image/upload/v1613749815/thumbnail_360x360_r_1_6a2049d13a.jpg",
-                provider_metadata: {
-                  public_id: "thumbnail_360x360_r_1_6a2049d13a",
-                  resource_type: "image",
-                },
-              },
-            },
-            provider: "cloudinary",
-            related: ["602fdf2d540c00269e056183"],
-            createdAt: "2021-02-19T15:50:15.686Z",
-            updatedAt: "2021-02-19T15:54:21.862Z",
-            __v: 0,
-            id: "602fde37540c00269e05616b",
           },
-          id: "602fdf2d540c00269e056183",
         },
         {
-          _id: "602fdf2d540c00269e056185",
-          __v: 0,
           image: {
-            _id: "602fde37540c00269e05616d",
-            name: "http://source.unsplash.com/random/360x360?r=2",
             alternativeText: "Imagem do topo de uma cidade grande",
-            caption: "Imagem do topo de uma cidade grande",
-            hash: "360x360_r_2_38651a645b",
-            ext: "",
-            mime: "image/jpeg",
-            size: 35.98,
-            width: 360,
-            height: 360,
             url: "https://res.cloudinary.com/dlizakp2a/image/upload/v1613749814/360x360_r_2_38651a645b.jpg",
-            provider_metadata: {
-              public_id: "360x360_r_2_38651a645b",
-              resource_type: "image",
-            },
-            formats: {
-              thumbnail: {
-                name: "thumbnail_http://source.unsplash.com/random/360x360?r=2",
-                hash: "thumbnail_360x360_r_2_38651a645b",
-                ext: "",
-                mime: "image/jpeg",
-                width: 156,
-                height: 156,
-                size: 7.97,
-                path: null,
-                url: "https://res.cloudinary.com/dlizakp2a/image/upload/v1613749815/thumbnail_360x360_r_2_38651a645b.jpg",
-                provider_metadata: {
-                  public_id: "thumbnail_360x360_r_2_38651a645b",
-                  resource_type: "image",
-                },
-              },
-            },
-            provider: "cloudinary",
-            related: ["602fdf2d540c00269e056185"],
-            createdAt: "2021-02-19T15:50:15.821Z",
-            updatedAt: "2021-02-19T15:54:21.968Z",
-            __v: 0,
-            id: "602fde37540c00269e05616d",
           },
-          id: "602fdf2d540c00269e056185",
         },
         {
-          _id: "602fdf2d540c00269e056188",
-          __v: 0,
           image: {
-            _id: "602fde37540c00269e05616f",
-            name: "http://source.unsplash.com/random/360x360?r=1",
             alternativeText: "Filme Kodak para câmeras antigas",
-            caption: "",
-            hash: "360x360_r_1_b3fcbf1d93",
-            ext: "",
-            mime: "image/jpeg",
-            size: 12.24,
-            width: 360,
-            height: 360,
             url: "https://res.cloudinary.com/dlizakp2a/image/upload/v1613749814/360x360_r_1_b3fcbf1d93.jpg",
-            provider_metadata: {
-              public_id: "360x360_r_1_b3fcbf1d93",
-              resource_type: "image",
-            },
-            formats: {
-              thumbnail: {
-                name: "thumbnail_http://source.unsplash.com/random/360x360?r=1",
-                hash: "thumbnail_360x360_r_1_b3fcbf1d93",
-                ext: "",
-                mime: "image/jpeg",
-                width: 156,
-                height: 156,
-                size: 3.77,
-                path: null,
-                url: "https://res.cloudinary.com/dlizakp2a/image/upload/v1613749815/thumbnail_360x360_r_1_b3fcbf1d93.jpg",
-                provider_metadata: {
-                  public_id: "thumbnail_360x360_r_1_b3fcbf1d93",
-                  resource_type: "image",
-                },
-              },
-            },
-            provider: "cloudinary",
-            related: ["602fdf2d540c00269e056188"],
-            createdAt: "2021-02-19T15:50:15.975Z",
-            updatedAt: "2021-02-19T15:54:21.989Z",
-            __v: 0,
-            id: "602fde37540c00269e05616f",
           },
-          id: "602fdf2d540c00269e056188",
         },
       ],
       metadata: {
         background: false,
-        _id: "602fdf2e540c00269e0561a4",
-        name: "gallery",
+
         section_id: "gallery",
-        __v: 0,
-        id: "602fdf2e540c00269e0561a4",
       },
-      __v: 2,
-      id: "602fdf2d540c00269e056175",
     });
 
     expect(sectionContObj.background).toBe(false);
